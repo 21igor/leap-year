a0a8851b89e716a0535b6782735d16c055f067f5
Igor Seleznov and Rony Nasr <igor@>
2016-06-08 10:03:18 -0700

    added files and front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,36 @@
-/*This file is for your custom js.  All yours*/
+// business logic
+var leapYear = function(year) {
+
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// user interface logic
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    // alert (Math.abs(year));
+    var result = leapYear(year);
+
+    if (!year){
+      alert ("Year can not be blank.")
+    } else {
+      if (year < 0){
+        $(".year").text(Math.abs(year) + " BC");
+      }else {
+        $(".year").text(year + " AD");
+      }
+
+      if (!result) {                 // same as writing if (result === false)
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+      $("#result").show();
+    }
+  });
+});
